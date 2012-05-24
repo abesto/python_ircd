5f2d8d1544efb02045df42ed0c6f3c4ce1c7834e
Zoltán Nagy <abesto@abesto.net>
2012-05-24 18:46:30 +0200

    Send topic on JOIN only if topic is set

--- commands/join.py
+++ commands/join.py
@@ -24,7 +24,8 @@ class JoinCommand(Command):
                 ret.append(M(channel.users,'JOIN', str(channel), prefix=self.user))
                 ret.append(RPL_NAMEREPLY(self.user, channel))
                 ret.append(RPL_ENDOFNAMES(self.user))
-                ret.append(RPL_TOPIC(self.user, channel))
+                if channel.topic is not None:
+                    ret.append(RPL_TOPIC(self.user, channel))
             except db.Error:
                 ret.append(ERR_NOSUCHCHANNEL(channel_name, self.user))
 
